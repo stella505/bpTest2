8831a7aa846dd04d213c97aa2a93040177a022e9
Miaoshi Wu <miaoshiwu@miaoshis-mbp.home>
2018-04-22 01:06:03 -0400

    Initial Commit

---
+++ ipfsProjectTest2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ipfsProjectTest2
+//
+//  Created by Miaoshi Wu on 4/22/18.
+//  Copyright © 2018 Miaoshi Wu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
